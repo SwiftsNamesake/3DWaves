cf08014eb0fd14ebb35de0b01b52a5964f298594
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-07-13 21:45:49 +0200

    Added Haddock, header, some TODOs, a few utility functions, parsers no longer discard trailing comments, changed structure of Model type, added several other types and refactored

--- Parsers.hs
+++ Parsers.hs
@@ -1,10 +1,13 @@
---
--- Wavefront - Parsers.hs
--- Parser and loader for WaveFront obj models
---
--- Jonatan H Sundqvist
--- February 8 2015
+-- |
+-- Module      : Southpaw.WaveFront.Parsers
+-- Description : descr
+-- Copyright   : (c) Jonatan H Sundqvist, February 8 2015
+-- License     : MIT
+-- Maintainer  : Jonatan H Sundqvist
+-- Stability   : experimental|stable
+-- Portability : POSIX (not sure)
 -- 
+-- Created February 8 2015-- Wavefront - Parsers.hs
 -- Migrated to separate project on February 21 2015
 
 -- TODO | - Appropriate container types (eg. bytestring, vector)
@@ -21,7 +24,6 @@
 --        - Caching (?)
 --        - Performance, profiling, optimisations
 --        - PrintfArg instances for the types defined in this module
---        - Decide on a public interface (exports)
 --        - Reconciling Cabal and hierarchical modules
 --        - Dealing with paths in lib statements (requires knowledge of working directories)
 --        - Move comments and specification to separate files (eg. README)
@@ -29,12 +31,26 @@
 --
 --        - Parser bugs
 --          -- Negative coordinates enclosed in parentheses
+--
+--        - Decide on a public interface (exports) (API)
+--          -- Model will be the main API type
+--          -- Export functions for working with the output data (eg. unzipIndices :: [(Int, Int, Int)] -> ([Int], [Int], [Int]))
 
--- SPEC | -
---        -
+-- SPEC | - 
+--        - 
 
 
 
+---------------------------------------------------------------------------------------------------
+-- GHC Extensions
+---------------------------------------------------------------------------------------------------
+{-# LANGUAGE UnicodeSyntax #-}
+
+
+
+---------------------------------------------------------------------------------------------------
+-- Section
+---------------------------------------------------------------------------------------------------
 module Southpaw.WaveFront.Parsers (parseOBJ, parseMTL, loadOBJ, loadMTL, MTL(), OBJ()) where
 
 
@@ -47,29 +63,31 @@ import Data.Char (isSpace)
 
 import Data.Either (rights, isLeft)
 
-import Text.Printf (printf)
-import System.IO (hFlush, stdout)
+import Text.Printf   (printf)
+import System.IO     (hFlush, stdout)
 import Control.Monad (forM_)
 -- import Control.Concurrent (threadDelay)
 
 import Southpaw.Utilities.Utilities (split)
 
-import qualified Data.Map as M
+import qualified Data.Map as Map
 
 
 
 ---------------------------------------------------------------------------------------------------
 -- Types
 ---------------------------------------------------------------------------------------------------
+-- OBJ parser types -------------------------------------------------------------------------------
 -- | Represents a single (valid) OBJ token
 --
 -- TODO: Polymorphic numerical types (?)
 -- TODO: Add context, metadata (eg. line numbers, filename) (?)
---
-data OBJToken = Vertex  Float Float Float |
-                Normal  Float Float Float |
-                Texture Float Float       |
-                Face [(Int, Int, Int)]    | -- TODO: Associate material with each face, handle absent indices
+-- TODO: Naming scheme (added OBJ prefix to prevent name clashes; cf. Face type)
+-- TODO: Comment token (preserve comments in parser output or remove them) (?)
+data OBJToken = OBJVertex  Float Float Float |
+                OBJNormal  Float Float Float |
+                OBJTexture Float Float       |
+                OBJFace [(Int, Int, Int)]    | -- TODO: Associate material with each face, handle absent indices
 
                 UseMTL String | --  
                 LibMTL String | -- TODO: Use actual MTL type
@@ -82,7 +100,7 @@ data OBJToken = Vertex  Float Float Float |
 -- |
 -- TODO: Use error type instead of String, allowing us to distinguish invalid data
 --       from eg. comments and blank lines (?)
-type OBJRow = Either String OBJToken
+type OBJRow = (Either String OBJToken, String)
 
 
 -- | Output type of the OBJ parser. Currently a list of line number and token (or error string) pairs
@@ -90,7 +108,61 @@ type OBJRow = Either String OBJToken
 -- TODO: Rename (?)
 -- TODO: Use Integral for line number (?)
 --
-type OBJ = [(Int, OBJRow)]
+type OBJ = [(Int, OBJRow, String)]
+
+
+-- MTL parser types -------------------------------------------------------------------------------
+-- | Represents a single (valid) MTL token
+--
+-- TODO: Is the alpha channel optional, ignored, disallowed?
+-- TODO: Include support for ('Ns', 'Ni', 'd', 'Tr', 'illum')
+-- 
+data MTLToken = Ambient  Float Float Float Float | -- Ka
+                Diffuse  Float Float Float Float | -- Kd
+                Specular Float Float Float Float | -- Ks
+
+        MapDiffuse  String | -- map_Kd
+        NewMaterial String   -- newmtl
+        deriving (Eq, Show)
+
+
+-- | Output type of the single-row MTL parser.
+type MTLRow = (Either String MTLToken, String)
+
+
+-- | Output type of the MTL parser. Currently a list of line number and token (or error string) pairs
+--
+-- TODO: Add type for processed MTL (eg. a map between names and materials)
+--
+type MTL = [(Int, MTLRow, String)] -- (line number, MTL token, comment)
+
+
+-- |
+type MTLTable = Map.Map String (Map.Map String Material)
+
+-- |
+type Colour = (Float, Float, Float, Float)
+
+
+-- |
+-- TODO: Do all materials have an ambient, a diffuse and a specular colour (?)
+-- TODO: Support more attributes (entire spec) (?)
+-- TODO: Lenses (?)
+data Material = Material { ambient :: Colour, diffuse :: Colour, specular, texture :: Maybe String }
+
+
+
+-- General types ----------------------------------------------------------------------------------
+type Vector num = (num, num, num) -- Queen Vectoria
+type Point  num = (num, num)      -- Haskell is no longer Point-free
+
+
+-- API types --------------------------------------------------------------------------------------
+-- |
+-- TODO: Validation (eg. length ivertices == length == ivertices == length itextures if length isn't 0)
+-- TOOD: Pack indices in a tuple (eg. indices :: [(Int, Int, Int)]) (?)
+-- TOOD: Use (String, String) for the names of the mtl file and material instead of Material (?)
+data Face = Face { indices :: ([Int], [Int], [Int]), material :: Material }
 
 
 -- | Abstract representation of an OBJ model with associated MTL definitions.
@@ -102,39 +174,14 @@ type OBJ = [(Int, OBJRow)]
 -- TODO: Perform index lookups (?)
 -- TODO: Reconsider the types (especially of the materials)
 --
-data Model = Model { vertices  :: [OBJToken],
-                     normals   :: [OBJToken],
-                     textures  :: [OBJToken],
-                     faces     :: [OBJToken],
-                     selects   :: [OBJToken],              -- TODO: Rename (UseMTL) (?) 
-                     materials :: M.Map String [MTLToken], -- TODO: Type synonym (?)
-                     groups    :: [OBJToken],
-                     objects   :: [OBJToken] } deriving (Show)
-
-
--- | Represents a single (valid) MTL token
---
--- TODO: Is the alpha channel optional, ignored, disallowed?
--- TODO: Include support for ('Ns', 'Ni', 'd', 'Tr', 'illum')
--- 
-data MTLToken = Ambient  Float Float Float | -- Ka
-                Diffuse  Float Float Float | -- Kd
-                Specular Float Float Float | -- Ks
-
-        MapDiffuse String | -- map_Kd
-        Material   String   -- newmtl
-        deriving (Eq, Show)
-
-
--- | Output type of the single-row MTL parser. 
-type MTLRow = Either String MTLToken
-
-
--- | Output type of the MTL parser. Currently a list of line number and token (or error string) pairs
---
--- TODO: Add type for processed MTL (eg. a map between names and materials)
---
-type MTL = [(Int, MTLRow)]
+data Model = Model { vertices  :: [Vector Float],
+                     normals   :: [Vector Float],
+                     textures  :: [Point  Float],
+                     faces     :: [Face],
+                     materials :: Map.Map String (Map.Map String Material), -- TODO: Type synonym (?)
+                     groups    :: Map.Map String (Int, Int),
+                     objects   :: Map.Map String (Int, Int)
+                   } deriving (Show)
 
 
 
@@ -151,10 +198,7 @@ type MTL = [(Int, MTLRow)]
 -- TODO: Should this function separate the various fields (eg. [(Vertices, Faces, Materials, Groups)] instead of [Maybe OBJToken])
 --
 parseOBJ :: String -> OBJ
-parseOBJ = zip [1..] . map parseOBJRow . lines -- . rows
-
--- I never knew pattern matching in list comprehensions could be used to filter by constructor
--- let rows = parseOBJ data in ([ v | @v(Vertex {}) <- rows], [ v | @v(Vertex {}) <- rows])
+parseOBJ = enumerate . map parseOBJRow . lines -- . rows
 
 
 -- | Generates a token given a single valid OBJ row, or an error value if the input is malformed. 
@@ -174,12 +218,12 @@ parseOBJ = zip [1..] . map parseOBJRow . lines -- . rows
 parseOBJRow :: String -> OBJRow -- Maybe OBJToken
 parseOBJRow ln
   | isComment ln || null ln = Left ln
-  | otherwise               = let (which:values) = words $ dropComment ln in case which of
-    "v"  -> vector Vertex values -- Vertex
-    "vn" -> vector Normal values -- Normal
+  | otherwise               = withoutComment ln $ \ tokens -> let (which:values) = words tokens in case which of
+    "v"  -> vector OBJVertex values -- Vertex
+    "vn" -> vector OBJNormal values -- Normal
     -- TODO: Clean this up
     -- TODO: More generic way of unpacking the right number of values and applying read (?)
-    "vt" -> let (x:y:[]) = values in Right $ Texture (read x) (read y) -- Texture
+    "vt" -> let (x:y:[]) = values in Right $ OBJTexture (read x) (read y) -- Texture
     -- TODO: Clean this up
     -- TODO: Handle invalid data (✓)
     -- TODO: Capture invalid vertex definitions (cf. sequence) (✓)
@@ -201,7 +245,7 @@ parseOBJRow ln
 -- |
 -- process the OBJ tokens
 parseMTL :: String -> MTL
-parseMTL = zip [1..] . map parseMTLRow . lines
+parseMTL = enumerate . map parseMTLRow . lines
 
 
 -- | 
@@ -210,15 +254,16 @@ parseMTL = zip [1..] . map parseMTLRow . lines
 parseMTLRow :: String -> MTLRow
 parseMTLRow ln
   | isComment ln || null ln = Left ln
-  | otherwise               = let (which:values) = words ln in case which of
+  | otherwise               = withoutComment ln $ \ tokens -> let (which:values) = words tokens in case which of
     "Ka" -> withChannels Ambient  values -- Ka
     "Kd" -> withChannels Diffuse  values -- Kd
     "Ks" -> withChannels Specular values -- Ks
     "map_Kd" -> withName MapDiffuse values -- map_Kd
     "newmtl" -> withName Material   values -- newmtl
     _        -> Left ln
-    where withChannels token (r:g:b:[]) = Right $ token (read r) (read g) (read b) -- TODO: No alpha channel (optional?) (?) (read a) 
-          withChannels _      _         = Left  $ "Pattern match failed"           -- TODO: No alpha channel (optional?) (?) (read a)
+    where withChannels token [r,g,b,a] = Right $ token (read r) (read g) (read b) (read a) --
+          withChannels token [r,g,b]   = Right $ token (read r) (read g) (read b) (1.0)    -- Alpha is 1.0 by default (use Maybe instead?)
+          withChannels _      _        = Left  $ "Pattern match failed"
 
           withName token (name:[]) = Right $ token name
           withName _      _        = Left  $ "Pattern match failed"
@@ -233,27 +278,27 @@ parseMTLRow ln
 -- TODO: Use a more efficient data structure (especially w.r.t indexing; cf. Vector)
 -- TODO: Consider preserving the indices (rather than generating a list of duplicated vertices).
 --       This would preserve space (in cases where vertices are often re-used), as well as being
---       very compatible with index arrays on graphics cards.
+--       very compatible with index buffers on graphics cards.
 --
-createModel :: OBJ -> ([String] -> M.Map String [MTLToken]) -> Model
-createModel modeldata retrieve = let tokens       = rights . map snd $ modeldata -- TODO: Vat do vee du viz ze dissidents, kommandant?
+-- TODO: Keep map of materials and list of textures in final output (inside model or as items in a tuple) (?)
+--
+-- I never knew pattern matching in list comprehensions could be used to filter by constructor
+-- let rows = parseOBJ data in ([ v | @v(Vertex {}) <- rows], [ v | @v(Vertex {}) <- rows])
+createModel :: OBJ -> MTLTable -> Model
+createModel modeldata materials = let tokens       = rights . map snd $ modeldata -- TODO: Vat do vee du viz ze dissidents, kommandant?
                                      theMaterials = retrieve [ name | UseMTL name   <- tokens ] -- Retrieve MTL data
-                                     theVertices  = [ vertex  | vertex@(Vertex{})   <- tokens ]
-                                     theNormals   = [ normal  | normal@(Normal{})   <- tokens ]
-                                     theTextures  = [ texture | texture@(Texture{}) <- tokens ]
                                      theFaces     = [ face    | face@(Face{})       <- tokens ]
                                      theGroups    = [ group   | group@(Face{})      <- tokens ]
                                      theObjects   = [ object  | object@(Face{})     <- tokens ]
                                      theSelects   = [ select  | select@(Face{})     <- tokens ]
                                      theObject    = [ object  | object@(Face{})     <- tokens ]
-                                 in Model { vertices  = theVertices,
-                                            normals   = theNormals,
-                                            textures  = theTextures,
-                                            faces     = theFaces,
-                                            selects   = theSelects, -- TODO: Rename (UseMTL) (?) 
-                                            groups    = theGroups,
-                                            objects   = theObjects,
-                                            materials = theMaterials } 
+                                  in Model { vertices  = [ vertex | vertex@(Vertex{})    <- tokens ],
+                                             normals   = [ normal | normal@(Normal{})    <- tokens ],
+                                             textures  = [ texture | texture@(Texture{}) <- tokens ],
+                                             faces     = theFaces,
+                                             groups    = theGroups,
+                                             objects   = theObjects,
+                                             materials = theMaterials } 
 
 
 -- Parsing utilities ------------------------------------------------------------------------------
@@ -268,17 +313,33 @@ isComment :: String -> Bool
 isComment = isPrefixOf "#" . dropWhile isSpace
 
 
--- |
+-- | Strips a trailing comment from an MTL or OBJ line.
 dropComment :: String -> String
 dropComment = takeWhile (/= '#')
+
+
+-- |
+takeComment :: String -> String
+takeComment = dropWhile (/= '#')
+
+
+-- |
+withoutComment :: String -> (String -> a) -> (a, String)
+withoutComment row parse = let (tokens, comment) = span (/= '#') in (parse tokens, comment)
+
+
+-- |
+enumerate :: [(token, comment)] -> [(Int, token, comment)]
+enumerate = zipWith [1..] prepend
+  where prepend n (token, comment) = (n, token, comment)
 
 
 -- | Splits a string into rows and filters out unimportant elements (empty lines and comments)
 -- NOTE: This function is probably obsolete due to comments being included by the parsers
 -- TODO: Higher order function for composing predicates
 rows :: String -> [String]
-rows = filter (\ ln -> not $ any ($ ln) [null, isComment]) . lines
-
+rows = filter (not . satisfiesAny [null, isComment]) . lines
+  where satisfiesAny predicates x = any ($ x) predicates
 
 -- |
 -- TODO: Use readMaybe (?)
