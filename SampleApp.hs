02071a5b568e1d4a09aead28851dd731d6207b08
Jonatan H Sundqvist <jonatanhsundqvist@gmail.com>
2015-07-25 15:22:03 +0200

    Refactoring, filled in shader programs

--- SampleApp.hs
+++ SampleApp.hs
@@ -10,7 +10,7 @@
 
 -- Created July 15 2015
 
--- TODO | - 
+-- TODO | - Break up the functionality of this program into several modules
 --        - 
 
 -- SPEC | -
@@ -26,8 +26,8 @@ module Southpaw.WaveFront.SampleApp where
 -- We'll need these
 ---------------------------------------------------------------------------------------------------
 import Graphics.Rendering.OpenGL
-import Graphics.Rendering.OpenGL.GL.Shaders
-import Graphics.Rendering.OpenGL.GL.ByteString (packUtf8)
+import Graphics.Rendering.OpenGL.GL.Shaders.ShaderObjects
+import Graphics.GLUtil
 
 import qualified Graphics.UI.GLFW as GLFW
 import Graphics.UI.GLFW as GLFW (MouseButton(..), KeyState(..), MouseButtonState(..))
@@ -35,6 +35,8 @@ import Graphics.UI.GLFW as GLFW (MouseButton(..), KeyState(..), MouseButtonState
 import System.FilePath (splitFileName, (</>))
 
 import Control.Monad (forM_, liftM, unless)
+import Control.Exception
+
 import Data.Maybe    (catMaybes)
 import Data.Either   (rights, lefts)
 import qualified Data.Map as Map
@@ -52,9 +54,12 @@ import Southpaw.Utilities.Utilities (numeral, gridM)
 -- Types
 ---------------------------------------------------------------------------------------------------
 -- | 
-type Buffers  = [(Normal3 GLfloat, WF.Material, [Vertex3 GLfloat])]
+type Buffers = [(Normal3 GLfloat, WF.Material, [Vertex3 GLfloat])]
 data AppState = AppState { _rotation :: (Double, Double), _mouse :: Maybe (Double, Double), _clientsize :: (Int, Int) } deriving (Show)
 
+data Mesh    = Mesh { vertices :: VBO,
+	                  normals  :: VBO,
+	                  colours  :: VBO}
 
 
 ---------------------------------------------------------------------------------------------------
@@ -63,6 +68,7 @@ data AppState = AppState { _rotation :: (Double, Double), _mouse :: Maybe (Doubl
 -- | 
 initOpenGL :: IO ()
 initOpenGL = do
+	--
 	diffuse  (Light 0) $= Color4  1.0 1.0 1.0 1.0
 	position (Light 0) $= Vertex4 1.0 1.0 4.5 0.0
 
@@ -70,7 +76,16 @@ initOpenGL = do
 	lighting          $= Enabled --
 
 	depthFunc $= Just Lequal
-	
+
+	-- Shader setup
+	let path = "C:\\Users\\Jonatan\\Desktop\\Haskell\\modules\\Southpaw\\lib\\Southpaw\\WaveFront"
+	eprogram <- loadShaderProgram (path </> "shader-vertex.glsl") (path </> "shader-pixel.glsl")
+	print eprogram
+	either
+	  (\ logs    -> printf "Unable to load shader program: %s" $ show logs)
+	  (\ program -> currentProgram $= Just program)
+	  (eprogram)
+
 	-- lineSmooth $= Enabled
 	-- blend      $= Enabled
 	-- blendFunc  $= (SrcAlpha, OneMinusSrcAlpha)
@@ -117,23 +132,36 @@ createBuffers model = triplets normals' (map WF.material faces') vertices'
 -- |
 -- TODO: Improve control flow
 -- TODO: Improve error checking (eg. which logs belong to which part, check errors at each stage?)
+-- TODO: Catch exceptions
+-- TODO: Program crashes when the source strings are empty
+-- TODO: Optional logging layer (?)
 createShaderProgram :: String -> String -> IO (Either [String] Program)
 createShaderProgram vsource psource = do
+	putStrLn "Creating shader program"
 	program <- createProgram
 	vshader <- createShader VertexShader
 	pshader <- createShader FragmentShader
 
+	putStrLn "Setting vertex shader source"
 	shaderSourceBS vshader $= packUtf8 vsource
-	shaderSourceBS pshader $= packUtf8 psource
 
+	putStrLn "Compiling vertex shader"
 	compileShader vshader
+
+	putStrLn "Setting fragment shader source"
+	shaderSourceBS pshader $= packUtf8 psource
 	compileShader pshader
 
+	-- putStrLn "Compiling shaders..."
+
 	vstatus <- get $ compileStatus vshader
+	print vstatus
 	pstatus <- get $ compileStatus pshader
+	print pstatus
 
 	if vstatus && pstatus
 		then do
+			putStrLn "Successfully compiled shaders. Linking program..."
 			mapM (attachShader program) [vshader, pshader]
 			linkProgram program
 			linked <- get $ linkStatus program
@@ -141,6 +169,116 @@ createShaderProgram vsource psource = do
 				then return $ Right program
 				else mapM get [shaderInfoLog vshader, shaderInfoLog pshader, programInfoLog program] >>= return . Left
 		else mapM (get . shaderInfoLog) [vshader, pshader] >>= return . Left
+
+
+--
+setShaderUniforms :: Program -> IO ()
+setShaderUniforms program = do
+	-- activeUniforms
+	let uMVMatrix = uniformLocation program "uMVMatrix" -- uniform mat4 uMVMatrix;
+	    uPMatrix  = uniformLocation program "uPMatrix" -- uniform mat4 uPMatrix;
+
+	uniform uMVMatrix $= undefined
+	uniform uPMatrix  $= undefined
+
+	return ()
+
+
+-- |
+loadShaderProgram :: String -> String -> IO (Either [String] Program)
+loadShaderProgram vpath ppath = do
+	vsource <- readFile vpath
+	psource <- readFile ppath
+
+	catch
+	  (createShaderProgram vsource psource)            --
+	  caught -- TODO: More elaborate exception message (?)
+	where
+	  caught :: IOException -> IO (Either [String] Program)
+	  caught e = return $ Left ["Unable to open file."]
+
+
+-- |
+render :: IORef AppState -> [Buffers] -> GLFW.WindowRefreshCallback
+render stateref buffers window = do
+	--
+	(rx, ry)   <- liftM _rotation   . readIORef $ stateref
+	(cxi, cyi) <- liftM _clientsize . readIORef $ stateref
+
+	let (cols, rows) = (3, 3)
+	gridM cols rows $ \ wxi wyi -> do
+
+		let (cx, cy) = (cint cxi, cint cyi)
+		    left     = ((wxi-1)*div cx cols)
+		    top      = ((wyi-1)*div cy rows)
+		    width    = (div cx cols)
+		    height   = (div cy rows)
+
+		clearColor $= Color4 (min 1.0 $ 0.5*float (wxi-1)) (min 1.0 $ 0.5*float (wyi-1)) (min 1.0 $ float (wxi-1)*float (wyi-1)*0.3) (1.0 :: GLfloat)
+		viewport   $=      (Position left top, Size width height)
+		scissor    $= Just (Position left top, Size width height)
+
+		-- TODO: Refactor with preservingMatrix (?)
+		matrixMode $= Modelview 0
+		loadIdentity
+		lookAt (Vertex3 0.0 0.0 5.0) (Vertex3 0.0 0.0 0.0) (Vector3 0.0 1.0 0.0)
+
+		translate    ((Vector3 0.0 (-2.0) (-4.0)) :: Vector3 GLfloat)
+		rotate (180.0 * (float rx/float cxi)) ((Vector3 0.0 1.0 0.0) :: Vector3 GLfloat)
+		rotate (180.0 * (float ry/float cyi)) ((Vector3 1.0 0.0 0.0) :: Vector3 GLfloat)
+
+		clear [ColorBuffer, DepthBuffer]
+		forM_ buffers renderModel
+	GLFW.swapBuffers window
+	where float :: (Real r, Fractional f) => r -> f -- TODO: Whuuuuuuut (monomorphism restriction)?
+	      float = realToFrac   
+
+	      cint :: (Num n,  Integral i) => i -> n -- TODO: Ditto (?)
+	      cint = fromIntegral
+
+
+-- |
+-- TODO: Colours and textures
+-- TODO: Arbitrary polygons
+-- TODO: Refactor, simplify
+renderModel :: Buffers -> IO ()
+renderModel buffers = forM_ buffers renderFace
+
+
+-- |
+renderMesh :: Mesh -> IO ()
+renderMesh mesh = do
+	drawArrays TriangleFan 0 (3*0) --
+	return ()
+-- drawArrays :: PrimitiveMode -> ArrayIndex -> NumArrayIndices -> IO ()
+-- bindVertexArrayObject :: StateVar (Maybe VertexArrayObject)
+-- 
+
+-- | (Normal)
+--
+-- TODO: Why just triangles (?)
+--
+renderFace :: (Normal3 GLfloat, WF.Material, [Vertex3 GLfloat]) -> IO ()
+renderFace (n, mat, vertices) =  renderPrimitive TriangleFan $ do color3f mat
+                                                                  normal n
+                                                                  mapM_ vertex vertices
+    where color3f mat = let (r,g,b,_) = WF.diffuse mat in (color $ Color3 (realToFrac r) (realToFrac g) (realToFrac b :: GLfloat))
+
+
+-- |
+showVertex (Vertex3 x y z) = show $ (show x, show y, show z)
+
+
+-- |
+showNormal (Normal3 x y z) = show $ (show x, show y, show z)
+
+
+-- Events -----------------------------------------------------------------------------------------
+
+-- | 
+-- animate fps = do
+	-- postRedisplay Nothing
+	-- addTimerCallback (div 1000 fps) (animate fps)
 
 
 -- |
@@ -174,103 +312,6 @@ onwindowresize stateref _ cx cy = do
 	modifyIORef stateref $ \ state -> state { _clientsize=(cx, cy) }     --
 	-- viewport $= (Position 0 0, Size (fromIntegral cx) (fromIntegral cy)) --
 	putStrLn $ unwords ["Window size is", show cx, "/", show cy]
-
-
--- |
-render :: IORef AppState -> [Buffers] -> GLFW.WindowRefreshCallback
-render stateref buffers window = do
-	--
-	(rx, ry)   <- liftM _rotation   . readIORef $ stateref
-	(cxi, cyi) <- liftM _clientsize . readIORef $ stateref
-
-	let (cols, rows) = (3, 3)
-
-	gridM cols rows $ \ wxi wyi -> do
-		let (cx, cy) = (cint cxi, cint cyi)
-		    left     = (wxi*div cx cols)
-		    top      = (wyi*div cy rows)
-		    width    = (div cx cols)
-		    height   = (div cy rows)
-		-- TODO: Clear up this horrible mess (✓)
-		clearColor $= Color4 (0.5*float wxi) (0.5*float wyi) (float wxi*float wyi*0.3) (1.0 :: GLfloat)
-		viewport   $=      (Position left top, Size width height)
-		scissor    $= Just (Position left top, Size width height)
-
-		-- TODO: Refactor with preservingMatrix (?)
-		matrixMode $= Modelview 0
-		loadIdentity
-		lookAt (Vertex3 0.0 0.0 5.0) (Vertex3 0.0 0.0 0.0) (Vector3 0.0 1.0 0.0)
-
-		translate    ((Vector3 0.0 (-2.0) (-4.0)) :: Vector3 GLfloat)
-		rotate (180.0 * (realToFrac rx/realToFrac cxi)) ((Vector3 0.0 1.0 0.0) :: Vector3 GLfloat)
-		rotate (180.0 * (realToFrac ry/realToFrac cyi)) ((Vector3 1.0 0.0 0.0) :: Vector3 GLfloat)
-
-		clear [ColorBuffer, DepthBuffer]
-		forM_ buffers renderModel
-	GLFW.swapBuffers window
-	where float = realToFrac   -- TODO: Whuuuuuuut (monomorphism restriction)?
-	      cint  = fromIntegral -- TODO: Ditto (?)
-
-
--- |
-renderSimple window = do
-	(width, height) <- GLFW.getFramebufferSize window
-	let ratio = fromIntegral width / fromIntegral height
-	
-	viewport $= (Position 0 0, Size (fromIntegral width) (fromIntegral height))
-	clear [ColorBuffer]
-	
-	matrixMode $= Projection
-	loadIdentity
-	ortho (negate ratio) ratio (negate 1.0) 1.0 1.0 (negate 1.0)
-	matrixMode $= Modelview 0
-	
-	loadIdentity
-	-- this is bad, but keeps the logic of the original example I guess
-	Just t <- GLFW.getTime
-	rotate ((realToFrac t) * 50) $ (Vector3 0 0 1 :: Vector3 GLdouble)
-	
-	renderPrimitive Triangles $ do
-		color  (Color3 1 0 0 :: Color3 GLdouble)
-		vertex (Vertex3 (negate 0.6) (negate 0.4) 0 :: Vertex3 GLdouble)
-		color  (Color3 0 1 0 :: Color3 GLdouble)
-		vertex (Vertex3 0.6 (negate 0.4) 0 :: Vertex3 GLdouble)
-		color  (Color3 0 0 1 :: Color3 GLdouble)
-		vertex (Vertex3 0 0.6 0 :: Vertex3 GLdouble)
-	GLFW.swapBuffers window
-
--- |
--- TODO: Colours and textures
--- TODO: Arbitrary polygons
--- TODO: Refactor, simplify
-renderModel :: Buffers -> IO ()
-renderModel buffers = forM_ buffers renderFace
-
-
--- | (Normal)
---
--- TODO: Why just triangles (?)
---
-renderFace :: (Normal3 GLfloat, WF.Material, [Vertex3 GLfloat]) -> IO ()
-renderFace (n, mat, vertices) =  renderPrimitive TriangleFan $ do color3f mat
-                                                                  normal n
-                                                                  mapM_ vertex vertices
-    where color3f mat = let (r,g,b,_) = WF.diffuse mat in (color $ Color3 (realToFrac r) (realToFrac g) (realToFrac b :: GLfloat))
-
-
--- |
-showVertex (Vertex3 x y z) = show $ (show x, show y, show z)
-
-
--- |
-showNormal (Normal3 x y z) = show $ (show x, show y, show z)
-
-
--- | 
--- animate fps = do
-	-- postRedisplay Nothing
-	-- addTimerCallback (div 1000 fps) (animate fps)
-
 
 ---------------------------------------------------------------------------------------------------
 
